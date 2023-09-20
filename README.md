66cfe6c672ea1d8b28346cebf4a300af12694af6
chenqj <chenqj@bokesoft.com>
2023-09-20 17:10:09 +0800

    nodejs-simple-dir

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nodejs-simple-dir
 简易nodejs模板
+
+xx 系统
+
+```
+npm i
+npm run test
+```
+
+打包
+```
+npm run build
+```
+
+生成测试报告
+```
+npm run cover
+```
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const md5 = require('md5');
+console.log('hello world!');
+
+const fileSystem = {
+  userConfig: {
+  },
+  appData: {
+
+  },
+  project: {
+    setProjectPath (projectPath) { // 切换项目时 setProjectPath
+      // todo
+    }
+  }
+};
+
+
+module.exports = fileSystem;
